6f15d1c5ba1bd0e4b0ac108c4043566083a6b017
Melle Wynia <contact@mellewynia.nl>
2019-07-27 14:19:17 +0200

    Add connection to Hasura

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
+<script context="module">
+  import gql from 'graphql-tag';
+  import { client } from './apollo';
+  import { subscribe } from 'svelte-apollo';
+  const AUTHOR_LIST = gql`
+    subscription {
+      profile {
+		id
+		name
+      }
+    }
+  `;
+  const authorsList = subscribe(client, { query: AUTHOR_LIST });
+</script>
+
 <script>
-	let name = 'Eve + svelte';
+	const name = 'Eve + svelte';
 </script>
 
 <h1>Hello {name}!</h1>
+
+
+<ul>
+  {#await $authorsList}
+    <li>Loading...</li>
+  {:then result}
+    {#each result.data.profile as profile}
+      <li>{profile.name}</li>
+    {:else}
+      <li>No authors found</li>
+    {/each}
+  {:catch error}
+    <li>Error loading authors: {error.message}</li>
+  {/await}
+</ul>
---
+++ src/apollo.js
@@ -0,0 +1,49 @@
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { getMainDefinition } from "apollo-utilities";
+
+console.log(localStorage.getItem('X-HASURA-ADMIN-SECRET'));
+
+const headers = {
+  'content-type': 'application/json',
+  'X-HASURA-ADMIN-SECRET': localStorage.getItem('X-HASURA-ADMIN-SECRET')
+};
+const getHeaders = () => {
+  return headers;
+};
+
+const cache = new InMemoryCache();
+
+const wsLink = new WebSocketLink({
+  uri: "wss://demo-hasura-graphql.herokuapp.com/v1/graphql",
+  options: {
+    reconnect: true,
+    lazy: true,
+    connectionParams: {
+      headers: getHeaders()
+    }
+
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: "https://demo-hasura-graphql.herokuapp.com/v1/graphql",
+  headers: getHeaders()
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+export const client = new ApolloClient({
+  link,
+  cache
+});
